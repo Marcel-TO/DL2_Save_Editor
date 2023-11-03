0e2009dda53fbbe3a0e961616b1fbc629bcff395
Marcel T.O <marcel@turobin-ort.at>
2023-11-03 16:49:19 +0100

    Managed to retreive the bytes from the file.

--- savegame-editor/src-tauri/src/main.rs
+++ savegame-editor/src-tauri/src/main.rs
@@ -1,15 +1,61 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-#[tauri::command]
+use std::{fs, io::Read};
+use log::info;
+
+#[tauri::command(rename_all = "snake_case")]
 fn greet(name: &str) -> String {
     format!("Hello, {}! You've been greeted from Rust!", name)
 }
 
+#[tauri::command(rename_all = "snake_case")]
+fn file_to_backend(file_path: &str) -> String {
+    info!("RUST: {}", file_path);
+    let result = format!("{}", file_path);
+    info!("{}", result);
+    result
+}
+
+#[tauri::command(rename_all = "snake_case")]
+fn get_content(file_path: &str) -> String {
+    match get_contents_from_file(file_path) {
+        Ok(bytes) => {
+            let formatted_string = bytes.iter()
+                .map(|byte| format!("{:02X}", byte))
+                .collect::<Vec<String>>()
+                .join(" ");
+            formatted_string
+        }
+        Err(_) => {
+            "Error: Unable to read file.".to_string()
+        }
+    }
+}
+
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .plugin(tauri_plugin_log::Builder::default().build())
+        .invoke_handler(tauri::generate_handler![
+            greet, 
+            file_to_backend,
+            get_content,
+            ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
+}
+
+fn get_contents_from_file(file_path: &str) -> Result<Vec<u8>, std::io::Error> {
+    let mut file = fs::File::open(file_path)?;
+
+    // Get the file size for allocating the byte array
+    let file_size = file.metadata()?.len() as usize;
+
+    // Create a byte array to hold the file contents
+    let mut file_contents = vec![0; file_size];
+
+    // Read the binary data from the file into the byte array
+    file.read_exact(&mut file_contents)?;
+    
+    Ok(file_contents)
 }
--- savegame-editor/src/components/load-save-speed-dialer/load-save-speed-dialer.tsx
+++ savegame-editor/src/components/load-save-speed-dialer/load-save-speed-dialer.tsx
@@ -1,93 +1,35 @@
-import * as React from 'react';
-import { styled } from '@mui/material/styles';
-import Box from '@mui/material/Box';
-import FormControl from '@mui/material/FormControl';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormLabel from '@mui/material/FormLabel';
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import Switch from '@mui/material/Switch';
-import SpeedDial, { SpeedDialProps } from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
-import FileCopyIcon from '@mui/icons-material/FileCopyOutlined';
-import SaveIcon from '@mui/icons-material/Save';
-import PrintIcon from '@mui/icons-material/Print';
-import ShareIcon from '@mui/icons-material/Share';
-
-const StyledSpeedDial = styled(SpeedDial)(({ theme }) => ({
-  position: 'absolute',
-  '&.MuiSpeedDial-directionUp, &.MuiSpeedDial-directionLeft': {
-    bottom: theme.spacing(2),
-    right: theme.spacing(2),
-  },
-  '&.MuiSpeedDial-directionDown, &.MuiSpeedDial-directionRight': {
-    top: theme.spacing(2),
-    left: theme.spacing(2),
-  },
-}));
-
-const actions = [
-  { icon: <FileCopyIcon />, name: 'Copy' },
-  { icon: <SaveIcon />, name: 'Save' },
-  { icon: <PrintIcon />, name: 'Print' },
-  { icon: <ShareIcon />, name: 'Share' },
-];
+import { info } from "tauri-plugin-log-api";
+import { useState } from 'react';
+import { invoke } from '@tauri-apps/api/tauri';
+import { open } from '@tauri-apps/api/dialog';
 
 export default function LoadSaveSpeedDial() {
-  const [direction, setDirection] =
-    React.useState<SpeedDialProps['direction']>('up');
-  const [hidden, setHidden] = React.useState(false);
+  const [selectedFile, setSelectedFile] = useState<String>("No file selected.");
+  const [content, setContent] = useState<String>("");
 
-  const handleDirectionChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setDirection(
-      (event.target as HTMLInputElement).value as SpeedDialProps['direction'],
-    );
-  };
+  async function handleSelectFile() {
+    let filepath = await open({
+      multiple: false
+    });
 
-  const handleHiddenChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    setHidden(event.target.checked);
-  };
+    if (filepath != null && !Array.isArray(filepath)) {
+      info(filepath);
+      await setSelectedFile( await invoke("file_to_backend", {file_path: filepath}));
+    }
+  }
+
+  async function handleTestIfContentAccessible() {
+    await setContent( await invoke("get_content", {file_path: selectedFile}));
+
+  }
 
   return (
-    <Box sx={{ transform: 'translateZ(0px)', flexGrow: 1 }}>
-      <FormControlLabel
-        control={
-          <Switch checked={hidden} onChange={handleHiddenChange} color="primary" />
-        }
-        label="Hidden"
-      />
-      <FormControl component="fieldset" sx={{ mt: 1, display: 'flex' }}>
-        <FormLabel component="legend">Direction</FormLabel>
-        <RadioGroup
-          aria-label="direction"
-          name="direction"
-          value={direction}
-          onChange={handleDirectionChange}
-          row
-        >
-          <FormControlLabel value="up" control={<Radio />} label="Up" />
-          <FormControlLabel value="right" control={<Radio />} label="Right" />
-          <FormControlLabel value="down" control={<Radio />} label="Down" />
-          <FormControlLabel value="left" control={<Radio />} label="Left" />
-        </RadioGroup>
-      </FormControl>
-      <Box sx={{ position: 'relative', mt: 3, height: 320 }}>
-        <StyledSpeedDial
-          ariaLabel="SpeedDial playground example"
-          hidden={hidden}
-          icon={<SpeedDialIcon />}
-          direction={direction}
-        >
-          {actions.map((action) => (
-            <SpeedDialAction
-              key={action.name}
-              icon={action.icon}
-              tooltipTitle={action.name}
-            />
-          ))}
-        </StyledSpeedDial>
-      </Box>
-    </Box>
+    <div>
+      <button onClick={handleSelectFile}>Select File</button>
+      <p>{selectedFile}</p>
+      
+      <button onClick={handleTestIfContentAccessible}>Content?</button>
+      <p>{content}</p>
+    </div>
   );
 }
