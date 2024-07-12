88c7a8d1b18359a9770316248915eefe09546e29
Marcel T.O <marcel@turobin-ort.at>
2024-07-12 17:55:21 +0200

    Added first Settings card demo for adding the game path

--- savegame-editor/src/pages/settings-page.tsx
+++ savegame-editor/src/pages/settings-page.tsx
@@ -1,6 +1,6 @@
-import { CircleUser, Menu, Package2, Search } from "lucide-react"
+import { CircleUser, HelpCircle, Menu, Package2, Search } from "lucide-react";
 
-import { Button } from "@/components/ui/button"
+import { Button } from "@/components/ui/button";
 import {
   Card,
   CardContent,
@@ -8,8 +8,8 @@ import {
   CardFooter,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card"
-import { Checkbox } from "@/components/ui/checkbox"
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -17,60 +17,57 @@ import {
   DropdownMenuLabel,
   DropdownMenuSeparator,
   DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
-import { Input } from "@/components/ui/input"
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
 
-
-import { NavbarComponent } from "@/components/custom/custom-navbar-component"
+import { NavbarComponent } from "@/components/custom/custom-navbar-component";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 export const SettingsPage = () => {
-    return (
-        <>
-        <div className="flex min-h-screen w-full flex-col">
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col">
         <NavbarComponent />
-      <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
-        <div className="mx-auto grid w-full max-w-6xl gap-2">
-          <h1 className="text-3xl font-semibold">Settings</h1>
-        </div>
-        <div className="mx-auto grid w-full max-w-6xl items-start gap-6 md:grid-cols-[180px_1fr] lg:grid-cols-[250px_1fr]">
-          <div className="grid gap-6">
-            <Card x-chunk="dashboard-04-chunk-1">
-              <CardHeader>
-                <CardTitle>Store Name</CardTitle>
-                <CardDescription>
-                  Used to identify your store in the marketplace.
-                </CardDescription>
-              </CardHeader>
-              <CardContent>
-                <form>
-                  <Input placeholder="Store Name" />
-                </form>
-              </CardContent>
-              <CardFooter className="border-t px-6 py-4">
-                <Button>Save</Button>
-              </CardFooter>
-            </Card>
+        <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
+          <div className="mx-auto grid w-full max-w-6xl gap-2">
+            <h1 className="text-3xl font-semibold">Settings</h1>
+          </div>
+          <div className="mx-auto grid w-full max-w-6xl gap-2">
             <Card x-chunk="dashboard-04-chunk-2">
               <CardHeader>
-                <CardTitle>Plugins Directory</CardTitle>
+                <div className="relative">
+                <CardTitle>DL2 Game Folder</CardTitle>
                 <CardDescription>
-                  The directory within your project, in which your plugins are
-                  located.
+                  The path where Dying Light 2 Game is installed.
                 </CardDescription>
+                <TooltipProvider>
+
+                <Tooltip>
+                  <TooltipTrigger>
+                    <HelpCircle className="absolute top-0 right-0 w-6 h-6 text-muted-foreground" />
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p className="text-sm">
+                      Due to a collaboration with @EricPlayZ, we are able to provide a way to bypass the CRC check. This will allow you to use and modify PC save files as well. The editor will deploy a .dll file to the game folder to bypass the CRC check.
+                    </p>
+                  </TooltipContent>
+                </Tooltip>
+                </TooltipProvider>
+                </div>
               </CardHeader>
               <CardContent>
                 <form className="flex flex-col gap-4">
                   <Input
-                    placeholder="Project Name"
-                    defaultValue="/content/plugins"
+                    placeholder="File path"
+                    defaultValue="/fullpath/where/game/is/installed"
                   />
                   <div className="flex items-center space-x-2">
-                    <Checkbox id="include" defaultChecked />
+                    <Checkbox id="CRC" defaultChecked />
                     <label
-                      htmlFor="include"
+                      htmlFor="CRC"
                       className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
                     >
-                      Allow administrators to change the directory.
+                      Allow to bypass CRC check.
                     </label>
                   </div>
                 </form>
@@ -79,10 +76,9 @@ export const SettingsPage = () => {
                 <Button>Save</Button>
               </CardFooter>
             </Card>
-          </div>
-        </div>
-      </main>
-    </div>
-        </>
-    )
-}
+            </div>
+        </main>
+      </div>
+    </>
+  );
+};
