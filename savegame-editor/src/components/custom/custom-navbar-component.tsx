1ae0f97791bc80501c8b35f0cd2063976f387f14
Marcel T.O <marcel@turobin-ort.at>
2024-08-23 00:24:20 +0200

    Added welcome page and adjusted routing

--- savegame-editor/src/components/custom/custom-navbar-component.tsx
+++ savegame-editor/src/components/custom/custom-navbar-component.tsx
@@ -91,7 +91,7 @@ export const NavbarComponent = () => {
         <TooltipProvider>
           <nav className="flex flex-col items-center gap-4 px-2 sm:py-5">
           <Link
-        to={"/"}
+        to={"/main"}
         className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
       >
         <Home className="h-4 w-4 transition-all group-hover:scale-110" />
---
+++ savegame-editor/src/pages/welcome-page.tsx
@@ -0,0 +1,66 @@
+import { NavbarComponent } from "@/components/custom/custom-navbar-component";
+import { Button } from "@/components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+} from "@/components/ui/dialog";
+import { useTheme } from "@/components/ui/theme-provider";
+import { AppSettings } from "@/models/settings-model";
+import { useNavigate } from "react-router-dom";
+
+type WelcomePageProps = {
+  appSettings: AppSettings;
+};
+
+export const WelcomePage = ({ appSettings }: WelcomePageProps) => {
+  const { setTheme } = useTheme();
+  const navigate = useNavigate();
+
+  const handleContinueWithEditor = () => {
+    setTheme(appSettings.theme.value);
+    navigate("/main");
+  };
+
+  return (
+    <>
+      <div className="flex min-h-screen w-full flex-col">
+        <NavbarComponent />
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <main className="grid flex-1 items-start gap-4 p-4">
+            <div>
+              <h1 className="text-3xl font-semibold mb-4">Welcome</h1>
+
+              <Dialog open={true}>
+                <DialogContent className="bg-card/70">
+                  <DialogHeader>
+                    <DialogTitle>
+                      Welcome to the Dying Light 2 Save Editor
+                    </DialogTitle>
+                  </DialogHeader>
+                  <DialogDescription>
+                    This tool allows you to take control of your game save,
+                    unlocking a realm of possibilities and customization. It is
+                    fully open source, so if you like to change something or
+                    want to collaborate to improve the experience for all.
+                  </DialogDescription>
+                  <DialogFooter>
+                    <Button
+                      onClick={handleContinueWithEditor}
+                      variant="outline"
+                    >
+                      Load settings and start Editing
+                    </Button>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
+            </div>
+          </main>
+        </div>
+      </div>
+    </>
+  );
+};
